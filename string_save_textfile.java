f21bf0c9ecf53edb79fef827d65197b1cb9be60b
imjhanC <jhan9502@gmail.com>
2023-01-09 05:00:11 +0800

    string save to textfile

---
+++ string_save_textfile.java
@@ -0,0 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+public class string_save_textfile {
+
+    public static void main(String[] args) {
+        String var1 ="test1";
+        String var2 ="test2";
+        String var3 ="test3";
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter("test.txt"));
+            writer.write(var1+","+var2+","+var3);
+            writer.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
