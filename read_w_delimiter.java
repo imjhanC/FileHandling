e495bd375576c680f55acbfa70932a8054b0b19c
imjhanC <jhan9502@gmail.com>
2023-01-09 04:38:17 +0800

    save scanner into textfile w delimiter

---
+++ read_w_delimiter.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class read_w_delimiter {
+    public static void main(String[] args) {
+        File file = new File("abc.txt"); // textfile here
+        Scanner sc = null;
+        try {
+         sc = new Scanner(file);
+         // Check if there is another line of input
+         while(sc.hasNextLine()){
+          String str = sc.nextLine();
+          parseLine(str);
+         }
+         
+        } catch (IOException  exp) {
+         exp.printStackTrace();
+        }
+        
+        sc.close();
+       }
+       
+       private static void parseLine(String str){
+        String book, author, price;
+        Scanner sc = new Scanner(str);
+        sc.useDelimiter("[|]");
+        // Check if there is another line of input
+        while(sc.hasNext()){
+         book = sc.next();
+         author = sc.next();
+         price = sc.next();
+         System.out.println("Book - " + book + " Author - " + author + 
+           " Price - " + price);  
+        }
+        sc.close();
+       } 
+      
+}
+   
